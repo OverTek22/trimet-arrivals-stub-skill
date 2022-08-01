43f1fd1127dce0300ae68fc29e7d742605c5f2cb
OverTek22 <109820719+OverTek22@users.noreply.github.com>
2022-08-01 15:59:11 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,12 +9,12 @@ class TrimetArrivalsStub(MycroftSkill):
     def handle_stub_arrivals_trimet(self, message):
         self.speak_dialog('which.stop')
     
-    @intent_handler('stop.3051.intent')
+    @intent_file_handler('stop.3051.intent')
     def handle_stop_3051(self, message):
         # stub file for stop 3051
         self.speak_dialog('stop.3051')
     
-    @intent_handler('stop.11771.intent')
+    @intent_file_handler('stop.11771.intent')
     def handle_stop_11771(self, message):
         # stub file for stop 11771
         self.speak_dialog('stop.11771')
