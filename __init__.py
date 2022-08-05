89e9aaaf1902354a91428dbdd4f01111343222fc
OverTek22 <109820719+OverTek22@users.noreply.github.com>
2022-08-05 15:00:19 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -84,12 +84,13 @@ class TrimetArrivalsStub(MycroftSkill):
                             bus_lines[ID]["Alerts"].append(alertInfo)
                         else:
                             bus_lines[ID]["Alerts"].append(alertInfo)
-        self.speak("There are %d buses going through this stop. They are: " % (len(bus_lines))
+        outputSentance = "There are %d buses going through this stop. They are: " % (len(bus_lines)
+        self.speak(outputSentance)
         for bus in bus_lines:
             self.speak(bus_lines[bus])
 
         # Example of user asking for line 45
-        # self.speak_dialog('speak.string', {'stuff': bus_lines["45"]["Arrivals"]})
+        self.speak_dialog('speak.string', {'stuff': bus_lines["45"]["Arrivals"]})
         # self.speak_dialog('stop.3051')
     
     @intent_handler('stop.11771.intent')
@@ -166,7 +167,7 @@ class TrimetArrivalsStub(MycroftSkill):
 
         # Example of user asking for line 47
         # self.speak_dialog('speak.string', {'stuff': bus_lines["47"]["Arrivals"]})
-        # self.speak_dialog('stop.11771')
+        self.speak_dialog('stop.11771')
 
 
 def create_skill():
