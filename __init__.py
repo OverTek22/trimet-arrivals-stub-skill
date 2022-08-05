a0a4ce96b1da1f5f7c2824cda13a9171b6876d21
OverTek22 <109820719+OverTek22@users.noreply.github.com>
2022-08-05 13:13:56 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,88 @@
 from mycroft import MycroftSkill, intent_handler
 
+from mycroft.util.parse import match_one
+#from mycroft.util.parse import fuzzy_match
+import mycroft.util
+from mycroft.audio import wait_while_speaking
+
 
 class TrimetArrivalsStub(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def get_arrivals(self, stopID):
+        # Base url
+        url = "https://trimet.org/ride/stop_schedule.html"
+        # Using the stop ID passed in, get schedules sorted by destinations
+        stop_url = "{}?stop_id={}&sort=destination".format(url,stopID)
+        
+        # Make a request
+        page = requests.get(stop_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #print(soup.prettify())
+
+        # Create top_items as empty list
+        bus_lines = {}  # List of dictionaries for the buses passing through this stop
+
+        # Extract and store in top_items according to instructions on the left
+        schedules = soup.select('div.scheduletimes')
+        for elem in schedules:
+            info = {}   # The dictionary of an individual bus
+
+            description = elem.select('h3')[0].text
+            # format description to be more readable by removing long spaces, tabs, and newlines
+            description = description.replace("\t", "").replace("\r", "").replace("\n", "")
+            description = description.replace("                ", "").replace("Next arrivals", "")
+
+            # Separate the line number away from the description
+            descriptionParts = description.partition("-")
+            info["ID"] = descriptionParts[0]
+            info["Description"] = descriptionParts[2]
+
+            buses = elem.select('ul.sortbydestination')
+            arrivalTimes = [] # List to contain arrival times for this bus
+            for bus in buses:
+                arrivalTime = bus.select('span')
+                for time in arrivalTime:
+                    arrivalTimes.append(time.text.strip())
+
+
+            info["Arrivals"] = arrivalTimes[:]
+            bus_lines[descriptionParts[0]] = info
+
+
+        # Get the alerts from the website as well
+        alertTextBlock = soup.select_one("div#alerts").text
+        # print(alertTextBlock)
+        alerts = alertTextBlock.split("\n\n")
+        for alert in alerts:
+            info2 = {}  # Temporary dictionary to store id number and text for alerts
+
+            if len(alert) == 0:
+                pass
+
+            else:
+                alert.replace("\n", "")
+                alertParts = alert.partition(":  ")
+
+                busID = alertParts[0]   # list of bus ids which have this alert
+                busID = busID.replace("\n", "")
+
+                alertInfo = alertParts[2] # text containing the actual alert
+
+               # split the string of ids into a list of ids
+                busIDS = busID.split(", ")
+                for ID in busIDS:
+                    #print("--" + ID)
+                    if ID in bus_lines: # add the alert if the bus goes to the stop
+                        if "Alerts" not in bus_lines[ID]:
+                            bus_lines[ID]["Alerts"] = []
+                            bus_lines[ID]["Alerts"].append(alertInfo)
+                        else:
+                            bus_lines[ID]["Alerts"].append(alertInfo)
+
+        # Example of user asking for line 45
+        self.speak_dialog('speak.string', bus_lines["45"]["Arrivals"])
 
     @intent_handler('stub.arrivals.trimet.intent')
     def handle_stub_arrivals_trimet(self, message):
@@ -11,8 +90,8 @@ class TrimetArrivalsStub(MycroftSkill):
     
     @intent_handler('stop.3051.intent')
     def handle_stop_3051(self, message):
-        
-        self.speak_dialog('stop.3051')
+        get_arrivals(3051)    # get the arrivals for given stop
+        # self.speak_dialog('stop.3051')
     
     @intent_handler('stop.11771.intent')
     def handle_stop_11771(self, message):
