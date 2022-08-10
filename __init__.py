ce4be271460bf2378250ab640a968b2a5444d21e
OverTek22 <109820719+OverTek22@users.noreply.github.com>
2022-08-10 13:59:05 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from mycroft import MycroftSkill, intent_handler
-from adapt.intent import IntentBuilder
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -7,31 +6,22 @@ class TrimetArrivalsStub(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
     
-    @intent_handler(IntentBuilder('next.arrivals').require('get.arrivals').one_of('stop.3051', 'stop.11771')).build()
-    def handle_get_arrivals(self, message):
-	    self.log.info("next arrivals adept intent")
-        utterance = message.data.get('utterance')
-        num = re.findall('[0-9]+', utterance)
-        self.log.info(num[0])
-        
-        self.speak("I heard")
-        self.speak(num[0])
-        
-        
     @intent_handler('stub.arrivals.trimet.intent')
     def handle_stub_arrivals_trimet(self, message):
         self.speak_dialog('which.stop')
-    '''
+    
     @intent_handler('stop.3051.intent')
     def handle_stop_3051(self, message):
         self.log.info(message.data.keys())
         utterance = message.data.get('utterance')
         num = re.findall('[0-9]+', utterance)
+        self.log.info(num)
         self.log.info(num[0])
-        
+
         self.speak("I heard")
+        self.speak(num)
         self.speak(num[0])
-        
+
         # Base url
         url = "https://trimet.org/ride/stop_schedule.html"
         # Using the stop ID passed in, get schedules sorted by destinations
@@ -101,7 +91,6 @@ class TrimetArrivalsStub(MycroftSkill):
         # Using the stop ID passed in, get schedules sorted by destinations
         stop_url = "{}?stop_id={}&sort=destination".format(url, 11771)
         # stop_url = "{}?stop_id={}&sort=destination".format(url, 10764)
-
         # Make a request
         page = requests.get(stop_url)
         soup = BeautifulSoup(page.content, 'html.parser')
@@ -158,7 +147,5 @@ class TrimetArrivalsStub(MycroftSkill):
             # Example of user asking for line 47
             self.speak_dialog('speak.string', {'intro': "The next bus arrives at", 'stuff': bus_lines[BusID]["Arrivals"][0]})
             # self.speak_dialog('stop.11771')
-
-'''
 def create_skill():
     return TrimetArrivalsStub()
